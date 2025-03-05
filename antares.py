12eaa58e1a1c6790ad82aac8b79f1d6971a15a95
Rreso <143344200+Rreso@users.noreply.github.com>
2025-03-05 19:59:47 +0700

    Update antares.py

--- antares.py
+++ antares.py
@@ -97,9 +97,9 @@ elif menu == "Lokasi":
     github_image_url_2 = "https://raw.githubusercontent.com/Rreso/monitoringcuaca/main/batam.jpg"  # Ganti dengan URL gambar kedua Anda
     
  
-        st.image(github_image_url, caption="Politeknik Negeri Batam", use_container_width=True)
+     st.image(github_image_url, caption="Politeknik Negeri Batam", use_container_width=True)
 
-        st.image(github_image_url_2, caption="Rooftop Gedung Utama", use_container_width=True)# Ganti dengan URL gambar Anda
+     st.image(github_image_url_2, caption="Rooftop Gedung Utama", use_container_width=True)# Ganti dengan URL gambar Anda
 
 elif menu == "Data Cuaca":
     st.title("Data Cuaca")
