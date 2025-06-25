f9b04acac2a6065783408fd659319597961d9411
Rreso <143344200+Rreso@users.noreply.github.com>
2025-06-25 21:31:45 +0700

    Update antares.py

--- antares.py
+++ antares.py
@@ -360,7 +360,7 @@ elif st.session_state.selected_menu == "Evaluasi Model 📋":
         st.write(f"📉 **MSE Decision Tree (Entropy)**: {mse_dt_entropy:.4f}")
         st.write(f"📉 **RMSE Decision Tree (Entropy)**: {rmse_dt_entropy:.4f}")
         st.write("🌳 **Decision Tree (Entropy)** - Akurasi per fold:", cv_scores_dt_entropy)
-        st.write(f"🌳 **Decision Tree (Entropy)** - Rata-rata Akurasi CV: {mean_cv_dt_entropy:.3f}"
+        st.write(f"🌳 **Decision Tree (Entropy)** - Rata-rata Akurasi CV: {mean_cv_dt_entropy:.3f}")
          
          st.subheader("🎲 Evaluasi Tambahan Naive Bayes")
         st.write("🎲 **Naïve Bayes** - Akurasi per fold (10-fold CV):", cv_scores_nb_alt)
